3ad9fc27da70babd1759f0cb62093f75a02571b5
StrongbyTime <1543847782@qq.com>
2016-10-10 12:09:48 +0800

    overload
    OVERLOAD

---
+++ shype.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class shype{
+	private double width,length;
+	private double radius;
+
+	shype(){
+
+	}
+	shype(double w,double l,double r){
+		width=w;
+		length=l;
+		radius=r;
+	}
+	double area(double width,double length){
+		return width*length;
+	}
+	double area(double radius){
+		return (Math.PI)*radius*radius;
+	}
+	 public static void main(String[] args) {
+		shype pattern=new shype();
+		Scanner input=new Scanner(Syystem.In);
+		pattern.width=input.nextDouble();
+		pattern.length=input.nextDouble();
+		pattern.radius=input.nextDouble();
+		System.out.println("矩形的面积为："+area(width,length));
+		System.out.println("圆形的面积为："+area(width,length));
+
+	}
+}
