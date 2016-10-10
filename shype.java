d0cc6e1d176894ba94e01ea6567320e84ffcf3d8
StrongbyTime <1543847782@qq.com>
2016-10-10 12:19:25 +0800

    Update shype.java
    correct some mistake

--- shype.java
+++ shype.java
@@ -19,12 +19,12 @@ public class shype{
 	}
 	 public static void main(String[] args) {
 		shype pattern=new shype();
-		Scanner input=new Scanner(Syystem.In);
+		Scanner input=new Scanner(Syystem.in);
 		pattern.width=input.nextDouble();
 		pattern.length=input.nextDouble();
 		pattern.radius=input.nextDouble();
-		System.out.println("矩形的面积为："+area(width,length));
-		System.out.println("圆形的面积为："+area(width,length));
-
+		System.out.println("矩形的面积为："+pattern.area(pattern.width,pattern.length));
+		System.out.println("圆形的面积为："+pattern.area(pattern.radius));
+		input.close();
 	}
 }
