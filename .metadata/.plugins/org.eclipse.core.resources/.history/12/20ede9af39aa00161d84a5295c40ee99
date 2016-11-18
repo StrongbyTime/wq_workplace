41b0a106bfa174f8f71859027869b5572ce8c938
StrongbyTime <1543847782@qq.com>
2016-11-18 09:38:18 +0800

    new exercise

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/12/20ede9af39aa00161d84a5295c40ee99
@@ -0,0 +1,9 @@
+package NoGengeric;
+
+public class get {
+
+	public static void main(String[] args){
+		get get1=new get();
+		System.out.println(get1.getClass().getName());
+	}
+}
---
+++ Exam/src/transpose/testtrabsposition.java
@@ -0,0 +1,12 @@
+package transpose;
+
+public class testtrabsposition {
+	public static void main(String[] args){
+		double[][] array={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+		transposition tran=new transposition();
+		System.out.println("转置前：");
+		tran.showArray(array);
+		System.out.println("转置后：");
+		tran.transpose(array);
+	}
+}
